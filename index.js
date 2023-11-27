c597ffe336267594766b48013f5245e076b259af
Mathieu Chauveau <chauveau.math@gmail.com>
2023-11-27 16:53:15 +0100

    quête 1.2 ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const port = 5000;
+const port = 5002;
 
 app
   .listen(port, () => {
--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
+
+const port = process.env.APP_PORT;
+
 const express = require("express");
 
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userController");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUserById);
 
 module.exports = app;
